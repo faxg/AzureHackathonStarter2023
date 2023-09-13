6d0f4eadaa09fcdef6ba2e3c3e58def2b719f0fd
Florian Georg <florian.alexander.georg@gmail.com>
2023-09-13 19:45:55 +0000

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# AzureHackathonStarter2023
-HackathonStarter Template for Azure related Hackathons. Uses Docusaurus for static pages and Azure Static Web Apps
+# HackZurich - 2023
+
+Website for HackZurich 2023
+
+JAM stack:
+- Website: Azure Static Web App (SWA)
+- Static site generator: Docusaurus
+- Content: Markdown
+
+
+TODO:
+- Links footer (MS Learn etc.)
+- Logo
+- Frontpage
+- Sidebar and structure
+- Announcements / Blog
+- Auth area with downloads
+
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,20 +6,20 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'HackZurich 2023 - Microsoft Azure Landing Page',
+  tagline: 'Build awesome stuff with Microsoft Azure OpeAI Services',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://green-stone-0a07a7303.3.azurestaticapps.net/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'faxg', // Usually your GitHub org/user name.
+  projectName: 'hackzurich2023', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/faxg/AzureHackathonStarter2023/tree/hackzurich2023/website',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/faxg/AzureHackathonStarter2023/tree/hackzurich2023/website',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -62,12 +62,12 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'HackZurich 2023',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'MSFT + HackZurich Logo',
+          src: 'img/hackzurich2023.png',
         },
         items: [
           {
@@ -76,7 +76,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -127,7 +127,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Thanks to all contributors - ${new Date().getFullYear()}. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
